a497e922c0b34347284314a5e3115f1c44bc1f66
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2023-05-07 12:25:39 +0200

    react cards are done. Used some CSS with Bulma

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
@@ -6,11 +7,43 @@ import SiriImage from "./images/siri.png";
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
 
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className="container">
+                <section class="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Alexa" 
+                                handle="@alexa99" 
+                                image={AlexaImage}
+                                description="At vero eos et accusam et justo duo dolores et ea rebum." 
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana32" 
+                                image={CortanaImage}
+                                description="At vero eos et accusam et justo duo dolores et ea rebum."  
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Siri" 
+                                handle="@siri01" 
+                                image={SiriImage}
+                                description="At vero eos et accusam et justo duo dolores et ea rebum."  
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
